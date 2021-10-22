129c22656469bab6f9b982cd17a96cac0d1ac138
Andrej <stefan.marjanovic@greatplacetowork.com>
2021-10-22 14:04:02 +0200

    adding selenium standalone

--- features/support/getBrowser.js
+++ features/support/getBrowser.js
@@ -1,5 +1,6 @@
 const seleniumWebdriver = require('selenium-webdriver');
 require('chromedriver');
+// SELENIUM_BROWSER=safari node example/google_search - comand line
 
 
 function getBrowser(browserName) {
@@ -7,4 +8,3 @@ function getBrowser(browserName) {
     return new seleniumWebdriver.Builder().forBrowser(browserName).build();
   }
   exports.browser = getBrowser('chrome');
-  
